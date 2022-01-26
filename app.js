48d57221a8ae21d7857f51abe2e1b8084c71a658
Shivanshu sood <Shvnshsood@gmail.com>
2022-01-26 07:36:12 -0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const btn = document.getElementById("btn");
+const color= document.querySelector(".color");
+
+
+
+btn.addEventListener("click" , function(){
+
+const randomNum = getrandomnum();
+
+document.body.style.backgroundColor = colors[randomNum];
+
+color.textContent = colors[randomNum];
+
+
+});
+
+
+function getrandomnum()
+{
+   
+    return Math.floor( Math.random() * colors.length);
+}
---
+++ hex.js
@@ -0,0 +1,29 @@
+const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+
+const btn = document.getElementById('btn');
+const color = document.querySelector('.color');
+
+btn.addEventListener('click' ,function(){
+
+let hexColor = '#'; 
+
+
+for(i=0;i<6;i++)
+{
+    hexColor += hex[getrandomnum()]
+}
+color.textContent = hexColor;
+document.body.style.backgroundColor = hexColor; 
+
+
+
+ 
+});
+
+
+function getrandomnum()
+{
+   
+    return Math.floor( Math.random() * hex.length);
+}
